8b1d2b7a43fa5fa67bdcb2f5fa22d79025d1dc0d
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-11-03 18:50:53 +0200

    osa4 4.2 config utility

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const blogSchema = mongoose.Schema({
@@ -15,8 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then(() => {
+mongoose.connect(config.MONGODB_URI).then(() => {
   logger.info("Connected to MongoDB")
 }).catch(error => logger.error(error.message))
 
@@ -41,7 +40,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
