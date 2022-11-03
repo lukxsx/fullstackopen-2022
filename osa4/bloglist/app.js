2c545b23b83bff39f468d1a9a99873e8f631cd34
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-11-03 19:11:55 +0200

    osa4 4.2 finished separating parts in modules

---
+++ osa4/bloglist/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI).then(() => {
+  logger.info("Connected to MongoDB")
+}).catch(error => logger.error(error.message))
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
+
--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,20 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogRouter = require('./controllers/blogs')
 
-mongoose.connect(config.MONGODB_URI).then(() => {
-  logger.info("Connected to MongoDB")
-}).catch(error => logger.error(error.message))
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
