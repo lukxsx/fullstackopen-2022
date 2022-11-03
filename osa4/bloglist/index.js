a7f246b01f92f12111339b65d561b52c704b4cf7
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-11-03 19:06:03 +0200

    osa4 4.2 models and controllers separation

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -5,15 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI).then(() => {
   logger.info("Connected to MongoDB")
@@ -21,24 +13,7 @@ mongoose.connect(config.MONGODB_URI).then(() => {
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
