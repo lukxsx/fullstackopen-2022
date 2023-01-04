3a30dac1470aa94a6d94c6ba20b7e4ac8cbf2dfb
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-01-04 18:41:14 +0200

    osa4 4.18 added jwt login

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./middleware')
 
 mongoose.connect(config.MONGODB_URI).then(() => {
@@ -17,6 +18,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
 
