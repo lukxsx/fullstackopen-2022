5a449a33fc845e55d30c8c36677b4c8f7abc8a37
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-01-07 16:56:34 +0200

    osa5 5.1 user login works

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,60 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      alert('Invalid username or password')
+      setUsername('')
+      setPassword('')
+    }
+  }
+
+  return (
+      <>
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button type="submit">login</button>
+          </div>
+        </form>
+      </>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +62,14 @@ const App = () => {
     )  
   }, [])
 
+  if (user === null) return <LoginForm setUser={setUser} />
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+        <h2>Blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
     </div>
   )
 }
