409cf4280ab70a6f1b2a826799b06d57ebaec57b
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-01-07 21:04:31 +0200

    osa5 5.7 show more info about blogs

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showAll, setShowAll] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} {' '}
+      <button onClick={() => setShowAll(!showAll)}>{showAll ? 'hide' : 'show'}</button>
+      {showAll && (
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
