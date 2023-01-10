08a8e7eb80d63bf31b7d5f31b5bf066238bde817
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-01-10 16:36:00 +0200

    6.15 Initialize anecdotes using redux thunk

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -2,16 +2,14 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-     .getAll().then(data => dispatch(setAnecdotes(data)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
