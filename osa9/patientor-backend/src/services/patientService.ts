837b069953071138dbf43295d3c46ecbd3a861fa
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-03-13 17:04:38 +0200

    9.11 Created patients API endpoint and service

---
+++ osa9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,16 @@
+import patients from "../../data/patients";
+import { NonSensitivePatientEntry } from "../types";
+
+const getPatients = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getPatients,
+};
