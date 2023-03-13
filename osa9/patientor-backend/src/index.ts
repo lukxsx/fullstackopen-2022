009c3763bec4df30a4ba1c9d2b1f804e47a684c3
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-03-13 16:54:47 +0200

    9.10 Created diagnoses endpoint and diagnoses service

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
+import diagnosesRouter from "./routes/diagnoses";
 
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
@@ -9,6 +13,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("pinged");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
