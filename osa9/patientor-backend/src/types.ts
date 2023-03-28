e25c0f50b4f9637eea647e6ccbe60e2b035242d7
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2023-03-28 17:20:13 +0300

    9.22 return entry data

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 
 export enum Gender {
   Male = "male",
@@ -22,6 +21,52 @@ export type Patient = {
   occupation: string;
   entries: Entry[];
 };
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface sickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface discharge {
+  date: string;
+  criteria: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: sickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NewPatientEntry = Omit<Patient, "id">;
 
